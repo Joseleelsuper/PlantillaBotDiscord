8b1cfbe30b911503d1fd2cf5d3bdad59dcd2cf31
a <tory002254@gmail.com>
2024-01-18 16:41:01 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,145 @@
+#|---------------------------------------------------------------|#
+#|   Nombre del programa: app.py                                 |#
+#|   Descripción: Plantilla de Bot para Discord.                 |#
+#|   @autor: José Gallardo Caballero                             |#
+#|   @date: 07/03/2023                                           |#
+#|   @version: v1.0.0                                            |#
+#|   Versiones:                                                  |#
+#|       v1.0.0 [07/03/2023]: Creación del programa.             |#
+#|---------------------------------------------------------------|#
+
+#Librerías
+    #Generales
+import traceback                    #Librería para el manejo de errores
+from datetime import datetime       #Librería para el manejo de fechas y horas
+from dotenv import load_dotenv      #Librería para el manejo de variables de entorno
+import os
+
+    #Discord
+import discord                      #Librería para el manejo de Discord
+from discord import app_commands    #Librería para el manejo de comandos de Discord
+from discord.ext import commands    #Librería para el manejo de comandos de Discord
+
+#Variables globales
+load_dotenv()                           #Carga las variables de entorno
+    #Generales
+GUILD_ID = os.getenv("GUILD_ID")        #ID del servidor
+CREADOR_ID = 379658818172354563         #ID del creador del bot
+VERSION = "v1.0.0"                      #Versión del bot
+
+    #Discord
+GUILD = discord.Object(id = GUILD_ID)   #Guild del servidor
+INTENTS = discord.Intents.all()         #Permisos del bot
+Color = discord.Color.orange()          #Color de la barra lateral de los embeds
+
+    #Bot
+TOKEN = os.getenv("TOKEN")                                          #Token del bot
+commands_bot = commands.Bot(command_prefix = "", intents = INTENTS) #Prefijo de los comandos del bot, en desuso
+PERMISO = 8                                                         #Nivel de permisos del bot
+
+#Definir la inicialización del bot
+class abot(discord.Client):
+    #Definir el estado del bot
+    def __init__(self):
+        super().__init__(intents=INTENTS, chunk_guilds_at_startup=True)                     #Declara el bot como cliente de Discord
+        self.token = TOKEN                                                                  #Declara el token del bot
+        self.bot = commands_bot                                                             #Declara el bot como cliente de Discord
+        self.synced = True                                                                  #Declara que el bot está sincronizado 
+        self.activity = discord.Activity(type=discord.ActivityType.listening, name="/help") #Declara la actividad del bot
+        self.status = discord.Status.online                                                 #Declara el estado del bot
+
+    #Mensaje de inicio
+    async def on_ready(self):
+        await tree.sync(guild = GUILD)              #Sincroniza los comandos del bot con los del servidor.
+        print("Bot conectado y listo para usar")    #Mensaje de inicio
+        print(f"Enlace de invitación: https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions={PERMISO}&scope=bot") #Enlace de invitación del bot
+
+#Definir el árbol de comandos
+bot = abot()
+tree = app_commands.CommandTree(bot)
+
+###################################################################
+###################################################################
+###################################################################
+
+#Funciones
+    #Función que checkea el envío correcto de comandos
+def check(interaction):
+    try:
+        if interaction.response.is_done():
+            print("\n")
+            print(f"El comando '{interaction.data['name']}' ha sido enviado a '{interaction.guild.name}' por '\
+                  {interaction.user.name}' a la hora '{interaction.created_at}'")
+    except:
+        print("\n\n")
+        traceback.print_exc()
+        print("\n\n")
+        print(f"Error al enviar el comando '{interaction.data['name']}' a '{interaction.guild.name}' a la hora '{interaction.created_at}'")
+
+###################################################################
+###################################################################
+###################################################################
+
+#Comandos
+    #Comando de ayuda
+    #Los comandos se ejecutan solamente en su servidor. Para que se ejecuten en cualquier servidor, quitar el parámetro 'guild = Guild'
+@tree.command(name = "help", description = "Muestra la lista de comandos del bot.", guild = GUILD)
+async def help(interaction):
+    
+    embed = discord.Embed(title = "Comandos", description = "Lista de comandos del bot.", color = Color)
+    embed.add_field(name = "/help", value = "Muestra esta lista de comandos.", inline = False)
+    embed.add_field(name = "/ping", value = "Muestra el ping del bot.", inline = False)
+    embed.add_field(name = "/info", value = "Muestra información sobre el bot.", inline = False)
+    embed.add_field(name = "/invite", value = "Muestra el enlace de invitación del bot.", inline = False)
+    embed.add_field(name = "/uptime", value = "Muestra el tiempo que lleva el bot encendido.", inline = False)
+    embed.set_footer(text = "Bot creado por <@{Creador_ID}>")
+    await interaction.response.send_message(embed = embed, ephemeral = True)
+    
+    check(interaction)
+###################################################################
+    #Comando de ping
+@tree.command(name = "ping", description = "Muestra el ping del bot.", guild = GUILD)
+async def ping(interaction):
+    
+    await interaction.response.send_message(f"Ping del bot: {round(bot.latency * 1000)}ms", ephemeral = True)
+    
+    check(interaction)
+###################################################################
+    #Comando de información
+@tree.command(name = "info", description = "Muestra información sobre el bot.", guild = GUILD)
+async def info(interaction):
+    
+    embed = discord.Embed(title = "Información", description = "Información sobre el bot.", color = Color)
+    embed.add_field(name = "Nombre", value = bot.user.name, inline = False)
+    embed.add_field(name = "ID", value = bot.user.id, inline = False)
+    embed.add_field(name = "Creador", value = f"<@{CREADOR_ID}>", inline = False)
+    embed.add_field(name = "Versión", value = f"{VERSION}", inline = False)
+    embed.add_field(name = "Librerías", value = "Discord.py", inline = False)
+    embed.add_field(name = "Lenguaje", value = "Python", inline = False)
+    embed.set_footer(text = "Bot creado por <@{CREADOR_ID}>")
+    await interaction.response.send_message(embed = embed, ephemeral = True)
+    
+    check(interaction)
+###################################################################
+    #Comando de invitación
+@tree.command(name = "invite", description = "Muestra el enlace de invitación del bot.", guild = GUILD)
+async def invite(interaction):
+
+    await interaction.response.send_message(f"Enlace de invitación: https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions={PERMISO}&scope=bot", ephemeral = True)
+
+    check(interaction)
+###################################################################
+    #Comando de uptime
+@tree.command(name = "uptime", description = "Muestra el tiempo que lleva el bot encendido.", guild = GUILD)
+async def uptime(interaction):
+    now = datetime.now()
+    uptime = now - bot_start_time
+    await interaction.response.send_message(f"Bot uptime: {uptime}", ephemeral = True)
+    
+    check(interaction)
+###################################################################
+###################################################################
+###################################################################
+if __name__ == "__main__":
+    bot_start_time = datetime.now()
+    bot.run(TOKEN)
---
+++ readme.md
@@ -0,0 +1,33 @@
+# Resumen del proyecto
+
+Este proyecto es un bot de Discord desarrollado en Python. El bot tiene la capacidad de realizar diversas acciones y responder a comandos específicos en un servidor de Discord.
+
+## Instalación de las dependencias
+
+Para instalar las dependencias necesarias, puedes utilizar el archivo `requirements.txt`. Ejecuta el siguiente comando en tu consola de comandos:
+```
+pip install -r requirements.txt
+``` 
+
+## Configuración del bot
+
+Para hacer que funcione, debes darle dos datos a través del fichero `variables.env`. Uno de ellos es el `token` y el otro el `guild_id`.
+
+`token` es el token del bot de Discord. Para obtener el token, debes crear una aplicación de Discord en el [portal de desarrolladores](https://discord.com/developers/applications). Luego, debes dirigirte a la sección de bot y crear un bot. Finalmente, debes copiar el token y pegarlo en el archivo `.env`.
+
+Sobre el guild_id, debes tener activado el modo desarrollador en discord. Puedes activarlo desde las opciones. Cuando lo tengas, dele click derecho al servidor y haga click a `Copiar ID del servidor`
+
+## Ejecución del bot
+
+Para ejecutar el bot, debes ejecutar el archivo `app.py`. Ejecuta el siguiente comando en tu consola de comandos:
+```
+python app.py
+``` 
+
+## Contribuciones
+
+Si deseas contribuir, puedes abrir un issue o enviar un pull request. Si deseas realizar un cambio importante, te sugiero que abras un issue para discutirlo.
+
+## Licencia
+
+Este proyecto está licenciado bajo la licencia MIT. Para más información, puedes leer el archivo `LICENSE`.
